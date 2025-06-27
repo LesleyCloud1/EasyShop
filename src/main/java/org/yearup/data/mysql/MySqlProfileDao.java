809653982aa5a95d49a9e12da72db96ebf9ad6a8
Lesley Solorzano <lsolorzano@appdev.yearup.org>
2025-06-27 09:51:50 -0500

    add ProfileController with GET and PUT endpoints, DAO updates for profile retrieval and updates. Tested on Postman.

--- src/main/java/org/yearup/data/mysql/MySqlProfileDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlProfileDao.java
@@ -21,9 +21,9 @@ public class MySqlProfileDao extends MySqlDaoBase implements ProfileDao
         String sql = "INSERT INTO profiles (user_id, first_name, last_name, phone, email, address, city, state, zip) " +
                 " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
-        try(Connection connection = getConnection())
+        try (Connection connection = getConnection())
         {
-            PreparedStatement ps = connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+            PreparedStatement ps = connection.prepareStatement(sql);
             ps.setInt(1, profile.getUserId());
             ps.setString(2, profile.getFirstName());
             ps.setString(3, profile.getLastName());
@@ -44,4 +44,72 @@ public class MySqlProfileDao extends MySqlDaoBase implements ProfileDao
         }
     }
 
+    @Override
+    public Profile getByUserId(int userId)
+    {
+        String sql = "SELECT * FROM profiles WHERE user_id = ?";
+
+        try (Connection connection = getConnection())
+        {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setInt(1, userId);
+
+            ResultSet rs = ps.executeQuery();
+            if (rs.next())
+            {
+                return mapRow(rs);
+            }
+            else
+            {
+                return null;
+            }
+        }
+        catch (SQLException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void update(int userId, Profile profile)
+    {
+        String sql = "UPDATE profiles SET " +
+                "first_name = ?, last_name = ?, phone = ?, email = ?, address = ?, city = ?, state = ?, zip = ? " +
+                "WHERE user_id = ?";
+
+        try (Connection connection = getConnection())
+        {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, profile.getFirstName());
+            ps.setString(2, profile.getLastName());
+            ps.setString(3, profile.getPhone());
+            ps.setString(4, profile.getEmail());
+            ps.setString(5, profile.getAddress());
+            ps.setString(6, profile.getCity());
+            ps.setString(7, profile.getState());
+            ps.setString(8, profile.getZip());
+            ps.setInt(9, userId);
+
+            ps.executeUpdate();
+        }
+        catch (SQLException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Profile mapRow(ResultSet row) throws SQLException
+    {
+        Profile profile = new Profile();
+        profile.setUserId(row.getInt("user_id"));
+        profile.setFirstName(row.getString("first_name"));
+        profile.setLastName(row.getString("last_name"));
+        profile.setPhone(row.getString("phone"));
+        profile.setEmail(row.getString("email"));
+        profile.setAddress(row.getString("address"));
+        profile.setCity(row.getString("city"));
+        profile.setState(row.getString("state"));
+        profile.setZip(row.getString("zip"));
+        return profile;
+    }
 }
